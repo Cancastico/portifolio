024b834f8848d699b5a3977f8a4444aad15239b4
Carlos Augusto <carlosaaugusto2002@outlook.com>
2024-08-08 23:57:11 -0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image';
+import Navbar from '../components/navbar/navbar';
+
+export default function Home() {
+  return (
+    <main className="w-full">
+      <Navbar></Navbar>
+    </main>
+  );
+}
---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,37 @@
+export default function Navbar() {
+  return (
+    <header className="flex flex-row justify-between items-center w-full px-[10%] h-20 bg-background-primary">
+
+      <div className="flex flex-row gap-1">
+        <span className="font-semibold text-primary text-lg">{`<C/>`}</span>
+        <p className="font-semibold text-lg">Cancastico</p>
+      </div>
+
+      <nav className="flex flex-row gap-20">
+        <ul className="flex flex-row gap-10">
+          <li>
+            <a className="text-black hover:text-primary hover:underline font-medium transition-all duration-200" href="#">Home</a>
+          </li>
+          <li>
+            <a className="text-black hover:text-primary hover:underline font-medium transition-all duration-100" href="#">Blog</a>
+          </li>
+          <li>
+            <a className="text-black hover:text-primary hover:underline font-medium transition-all duration-100" href="#">Projetos</a>
+          </li>
+          <li>
+            <a className="text-black hover:text-primary hover:underline font-medium transition-all duration-100" href="#">Contato</a>
+          </li>
+        </ul>
+
+        <ul className="flex flex-row gap-10">
+          <li>
+            <a className="text-black hover:text-primary hover:underline font-medium transition-all duration-100" href="#">Discord</a>
+          </li>
+          <li>
+            <a className="text-black hover:text-primary hover:underline font-medium transition-all duration-100" href="#">Github</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
