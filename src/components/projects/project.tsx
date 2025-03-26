5649f6cd0650f65cd621ca0d06f18d130033da9c
Carlos Augusto <carlosaaugusto2002@outlook.com>
2025-03-26 16:15:46 -0300

    updt

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -1,23 +1,26 @@
 import Image, { StaticImageData } from "next/image"
 import todo from '@/../public/Projetos/todo-task.png';
-import cms from "@/../public/Cms.png"
+import coffee from "@/../public/paymecoffee.jpg"
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
+import { Github } from "lucide-react";
 
 export default function Projects() {
 
-  const projects: { title: string, description: string, link: string, imagem: StaticImageData }[] = [
+  const projects: { title: string, description: string,github_link:string, link: string, imagem: StaticImageData }[] = [
     {
       title: "Todo Task",
       description: "Projeto em que fiz uma lista de Tarefas e que posso atualizar suas tarefas",
       imagem: todo,
+      github_link:'https://github.com/Cancastico/Todo',
       link: "https://todo-cancasticos-projects.vercel.app/"
     },
     {
-      title: "CMS de Usuarios",
-      description: "Projeto pra gerenciar Usuarios e Empresas dentro de um Saas",
-      imagem: cms,
-      link: "https://github.com/Cancastico/cms-user-company"
+      title: "Pague-me um Café",
+      description: "Projeto de integração com a API do Mercado Pago para permitir pagamentos online.",
+      imagem: coffee,
+      github_link:'https://github.com/Cancastico/mercado-pago-integracao',
+      link: "https://mercado-pago-integracao.vercel.app/"
     },
   ]
   return (
@@ -31,28 +34,35 @@ export default function Projects() {
         {projects.map((project, index) => {
           return (
             <div key={index} className="w-[18rem] md:w-[20rem] rounded-lg   shadow-lg shadow-black/30 dark:shadow-white/10 dark:border-slate-700 border-slate-400 border-[1px] transition-all duration-300 ease-in-out transform hover:scale-105 hover:cursor-pointer">
-              <a
-                target="_blank"
-                href={project.link}
-                className="w-full hover:scale-105"
-              >
-                <Image
-                  className="w-full h-full object-cover rounded-t-lg "
-                  src={project.imagem}
-                  alt="todo image"
-                />
-                <div className="w-full pt-3 pb-0 bg-white dark:bg-zinc-500 rounded-b-lg flex flex-col gap-3">
-                  <h1 className="w-full px-3 font-semibold text-lg dark:text-white bg-white dark:bg-zinc-500"> {project.title}</h1>
-                  <p className="w-[100%] h-[4rem] px-3 pb-2 bg-white dark:text-white dark:bg-zinc-500 text-justify text-sm text-background-dark/80">{project.description}</p>
-                  <div>
 
-                    <Separator className="w-full bg-zinc-600"></Separator>
-                    <div className="w-full flex flex-col items-end py-2 pr-5 rounded-b-lg bg-white dark:bg-zinc-500">
+              <Image
+                className="w-full h-full max-h-36 object-cover rounded-t-lg "
+                src={project.imagem}
+                alt="todo image "
+              />
+              <div className="w-full pt-3 pb-0 rounded-b-lg flex flex-col gap-3">
+                <h1 className="w-full px-3 font-semibold text-lg dark:text-white "> {project.title}</h1>
+                <p className="w-[100%] h-[4rem] px-3 pb-2  dark:text-white  text-justify text-sm text-background-dark/80">{project.description}</p>
+                <div>
+
+                  <Separator className="w-full bg-zinc-600"></Separator>
+                  <div className="w-full flex flex-row justify-end py-2 pr-5 rounded-b-lg gap-1">
+                    <a
+                      target="_blank"
+                      href={project.github_link}
+                    >
+
+                      <Button className="text-white font-semibold hover:bg-primary/70 transition-all ease-in-out round" > <Github size={16} /></Button>
+                    </a>
+                    <a
+                      target="_blank"
+                      href={project.link}
+                    >
                       <Button className="w-[9rem] text-white font-semibold hover:bg-primary/70 transition-all ease-in-out"> Acessar </Button>
-                    </div>
+                    </a>
                   </div>
                 </div>
-              </a>
+              </div>
             </div>
           )
 
